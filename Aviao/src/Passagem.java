fc6df757463705e371a38c2ffec9aaf268ab7b6f
Lucas Muniz <87603728+lucasMunizt@users.noreply.github.com>
2022-12-07 22:06:39 -0300

    Add files via upload

---
+++ Aviao/src/Passagem.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Random;
+public class Passagem{
+
+    private Voo voo;
+
+    public Voo getVoo() {
+        return voo;
+    }
+
+    public void setVoo(Voo voo) {
+        this.voo = voo;
+    }
+}
